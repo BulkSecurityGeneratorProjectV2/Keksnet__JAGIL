6e53e6391228b521480357b299226a3b31518862
Neo8 <56829999+Keksnet@users.noreply.github.com>
2022-08-18 21:19:28 +0200

    updated docs

---
+++ src/de/neo/jagil/ui/impl/UIClick.java
@@ -0,0 +1,33 @@
+package de.neo.jagil.ui.impl;
+
+import org.bukkit.event.inventory.ClickType;
+
+import java.awt.*;
+
+public class UIClick<T> {
+
+    private final Class<T> uiType;
+
+    private final Point click;
+
+    private final ClickType clickType;
+
+    public UIClick(Class<T> uiType, Point click, ClickType clickType) {
+        this.uiType = uiType;
+        this.click = click;
+        this.clickType = clickType;
+    }
+
+    public Class<T> getUiType() {
+        return uiType;
+    }
+
+    public Point getClick() {
+        return click;
+    }
+
+    public ClickType getClickType() {
+        return clickType;
+    }
+
+}
