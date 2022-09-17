32ca681bfd6a296d54b7bf0311293e37a7fc4cea
Neo8 <56829999+Keksnet@users.noreply.github.com>
2022-09-17 20:58:00 +0200

    entity in ui action

--- src/de/neo/jagil/ui/impl/UIAction.java
+++ src/de/neo/jagil/ui/impl/UIAction.java
@@ -1,21 +1,26 @@
 package de.neo.jagil.ui.impl;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.event.inventory.ClickType;
 
 import java.awt.*;
 
 public class UIAction<T> {
 
+    private final Entity entity;
     private final Class<T> uiType;
-
     private final Point click;
-
     private final ClickType clickType;
 
-    public UIAction(Class<T> uiType, Point click, ClickType clickType) {
+    public UIAction(Entity entity, Class<T> uiType, Point click, ClickType clickType) {
+        this.entity = entity;
         this.uiType = uiType;
         this.click = click;
         this.clickType = clickType;
+    }
+
+    public Entity getEntity() {
+        return entity;
     }
 
     public Class<T> getUiType() {
